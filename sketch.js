e4ba19b2467bfd3cd77eeb7646e79fc8c785eea7
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2022-08-15 15:02:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var fruit;
+function preload(){
+  backgroundImg = loadImage("background.png");
+ fruit=loadImage("melon.png");
+ g=loadImage("basket.png")
+}
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+
+ 
+
+}
+function draw() {
+  background(backgroundImg); 
+  ellipseMode(RADIUS)
+  Engine.update(engine);
+  
+
+}
